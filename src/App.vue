d4c7dbf09e5b1608fb77961b36e6ddec6d30245a
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-11-25 17:37:52 -0300

    Router view

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Navbar></Navbar>
+  <router-view/>
+  <Footer></Footer>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue';
+  import Footer from './components/Footer.vue';
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>
